119d3ff8dbb15e925a6044dbfaccf06262c54666
Guillaume Revaillot <revaillot@archos.com>
2018-09-04 17:52:00 +0200

    make build

--- src/main/java/org/stm32flash/STM32HexFirmwareParser.java
+++ src/main/java/org/stm32flash/STM32HexFirmwareParser.java
@@ -1,13 +1,13 @@
 package org.stm32flash;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+//import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.IOException;
 
 public class STM32HexFirmwareParser extends STM32FirmwareParser {
     public STM32HexFirmwareParser(String path) {
         super(path);
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
     }
 
     @Override
